dbb4b107a2cee7d1d8aa420de228a866911693eb
Nei Junio Nogueira Gomes <152228340+NeiJunio@users.noreply.github.com>
2025-02-25 16:52:25 -0300

    tipos de dados: hora, data e strings (concatenação e parâmetros)

--- C-Sharp/estruturas/tiposDeDados/Program.cs
+++ C-Sharp/estruturas/tiposDeDados/Program.cs
@@ -1,3 +1,5 @@
+using System.Text; // para usar a String.Builder
+
 namespace C-Sharp.estruturas.tiposDeDados; 
 
 public class Program
@@ -12,21 +14,88 @@ public class Program
         sbyte numeroSbyte = 127;
         int numeroUnderline = 1_000;
 
+
         // PONTOS FLUTUANTES
         // sintaxe: tipo_de_dado nome_da_variável = valor;
         float numeroFloat = 7.15f;
         double numeroDouble = 7.15;
         decimal numeroDecimal = 7.15m;
 
+
         // BOOLEANOS
         bool verdadeiro = true;
         bool falso = false;
+
 
         // CHAR
         char caracter = 'a';
 
         // STRING
         string texto = "Hello World!";
+        
+        char primeiraLetraDoTexto = texto[0]; 
+        Console.WriteLine(primeiraLetraDoTexto); // vai imprimir H
+
+        string meuNome = "         Nei           ";
+        string nomeFormatado = meuNome.Trim(); // remove os espaços em branco do inicio e fim, e devolve uma nova string
+
+        Console.WriteLine(meuNome); // vai imprimir           Nei          ;
+        Console.WriteLine(nomeFormatado); // vai imprimir Nei;
+
+
+        bool comecaComALetraN = nomeFormatado.StartWith("N");
+        Console.WriteLine(comecaComALetraN); // vai imprimir True
+
+
+        bool terminaComALetraI = nomeFormatado.EndsWith("i");
+        Console.WriteLine(terminaComALetraI); // vai imprimir True
+
+
+        string substituindoCaracterComReplace = nomeFormatado.Replace("ei", "EI");
+        Console.WriteLine(substituindoCaracterComReplace); // vai imprimir NEI;
+
+
+        string textoTeste = "Olá, mundo";
+        bool existe = textoTeste.Contains("mundo");
+        Console.WriteLine(existe); // vai imprimir True
+
+
+        string textoTeste2 = "Olá, mundo";
+        bool existe2 = textoTeste2.Equals("mundo");
+        Console.WriteLine(existe2); // vai imprimir False, pois não é 100% igual
+
+
+        // CONCATENÇÃO DE STRINGS
+        string texto1 = "a primeira frase";
+        string texto2 = "e a segunda frase";
+
+        string concatenado = texto1 + " " + texto2;
+        Console.WriteLine(concatenado); // vai imprimir a primeira frase e a segunda frase;
+
+        string concatenadoComInterpolacao = $"{texto1} {texto2}";
+
+
+        // STRINGBUILDER
+        String.Builder stringBuilder = new string.Builder();
+        stringBuilder.Append(texto1);
+        stringBuilder.Append(texto2);
+
+        string resultadoStringBuilder = stringBuilder.ToString();
+
+        Console.WriteLine(resultadoStringBuilder); // vai imprimir "a primeira frase e a segunda frase";
+
+
+        // STRINGS COM PARÂMETROS
+        string textoComParametro = "O usuário {0} gosta de {1}";
+        string resultadoStringComParametro = string.Format(textoComParametro, "Nei", "estudar")
+        Console.WriteLine(resultadoStringComParametro); // vai imprimir "O usuário Nei gosta de estudar"
+
+
+
+        // EVITANDO PROBLEMAS COM BARRA
+        string caminho = "C:\teste"; // \t representa um tab, então não vai imprimir corretamente
+        string caminho = "C:\\teste"; 
+        string caminho = @"C:\teste"; 
     }
 }
 
---
+++ C-Sharp/estruturas/tiposDeDados/Program2.cs
@@ -0,0 +1,86 @@
+
+namespace C-Sharp.estruturas.tiposDeDados; 
+
+public class Program2
+{
+    static void Main()
+    {
+        DateOnly dia = new DateOnly(2025, 2, 25);
+
+        string diaEmTextoToShort = dia.ToShortDateString(); // saída: 25-2-2025 
+        string diaEmTextoToLong = dia.ToLongDateString(); // saída: Segunda-Feria, 02 de fevereiro de 2025 
+
+        string diaEmTexto = dia.ToString(new CultureInfo("pt-BR")); // traduzindo a data para português-BR  --> saída: 25/02/2025
+
+        Console.WriteLine(diaEmTexto);
+
+
+        @*----------------------------------------------------------------------------------*@
+        DateTime dia = new DateTime(2008, 6, 15, 21, 15, 07);
+
+        string formatoPadrao = dia.ToString(new CultureInfo("pt-BR")); // saída: 15/06/2008
+        Console.WriteLine(formatoPadrao);
+
+        string formatoCurto = dia.ToString("d", new CultureInfo("pt-BR")); // saída: 15/06/2008
+        Console.WriteLine(formatoCurto);
+
+        string formatoLongo = dia.ToString("D", new CultureInfo("pt-BR")); // saída: domingo, 15 de junho de 2008
+        Console.WriteLine(formatoLongo);
+
+        string formatoDataHoraCurta = dia.ToString("f", new CultureInfo("pt-BR")); // saída: domingo, 15 de junho de 2008 21:15
+        Console.WriteLine(formatoDataHoraCurta);
+
+        string formatoDataHoraLonga = dia.ToString("F", new CultureInfo("pt-BR")); // saída: domingo, 15 de junho de 2008 21:15:07
+        Console.WriteLine(formatoDataHoraLonga);
+
+        string formatoDataHoraCompacta = dia.ToString("g", new CultureInfo("pt-BR")); // saída: 15/06/2008 21:15
+        Console.WriteLine(formatoDataHoraCompacta);
+
+        string formatoDataHoraCompleta = dia.ToString("G", new CultureInfo("pt-BR")); // saída: 15/06/2008 21:15:07
+        Console.WriteLine(formatoDataHoraCompleta);
+
+        string formatoMesDia = dia.ToString("m", new CultureInfo("pt-BR")); // saída: 15 de junho
+        Console.WriteLine(formatoMesDia);
+
+        string formatoISO8601 = dia.ToString("o", new CultureInfo("pt-BR")); // saída: 2008-06-15T21:15:07.0000000
+        Console.WriteLine(formatoISO8601);
+
+        string formatoRFC1123 = dia.ToString("R", new CultureInfo("pt-BR")); // saída: dom, 15 jun 2008 21:15:07 GMT
+        Console.WriteLine(formatoRFC1123);
+
+        string formatoAnoMes = dia.ToString("y", new CultureInfo("pt-BR")); // saída: junho de 2008
+        Console.WriteLine(formatoAnoMes);
+
+        string formatoHoraCurta = dia.ToString("t", new CultureInfo("pt-BR")); // saída: 21:15
+        Console.WriteLine(formatoHoraCurta);
+
+        string formatoHoraLonga = dia.ToString("T", new CultureInfo("pt-BR")); // saída: 21:15:07
+        Console.WriteLine(formatoHoraLonga);
+
+        string formatoUniversal = dia.ToString("u", new CultureInfo("pt-BR")); // saída: 2008-06-15 21:15:07Z
+        Console.WriteLine(formatoUniversal);
+
+        string formatoUniversalCompleto = dia.ToString("U", new CultureInfo("pt-BR")); // saída: segunda-feira, 16 de junho de 2008 00:15:07
+        Console.WriteLine(formatoUniversalCompleto);
+
+        Console.WriteLine();
+
+        string formatoPersonalizado1 = dia.ToString("h:mm:ss.ff t", new CultureInfo("pt-BR")); // saída: 9:15:07.00 P
+        Console.WriteLine(formatoPersonalizado1);
+
+        string formatoPersonalizado2 = dia.ToString("d MMM yyyy", new CultureInfo("pt-BR")); // saída: 15 jun 2008
+        Console.WriteLine(formatoPersonalizado2);
+
+        string formatoPersonalizado3 = dia.ToString("HH:mm:ss.f", new CultureInfo("pt-BR")); // saída: 21:15:07.0
+        Console.WriteLine(formatoPersonalizado3);
+
+        string formatoPersonalizado4 = dia.ToString("dd MMM HH:mm:ss", new CultureInfo("pt-BR")); // saída: 15 jun 21:15:07
+        Console.WriteLine(formatoPersonalizado4);
+
+        string formatoPersonalizado5 = dia.ToString(@"\Mês\: M", new CultureInfo("pt-BR")); // saída: Mês: 6
+        Console.WriteLine(formatoPersonalizado5);
+
+        string formatoPersonalizado6 = dia.ToString("HH:mm:ss.ffffzzz", new CultureInfo("pt-BR")); // saída: 21:15:07.0000-03:00
+        Console.WriteLine(formatoPersonalizado6);
+    }
+}
