c3e98fbd5aa61e62f38df4368c5993497b70f3a2
NeiJunio <neijunio50@gmail.com>
2025-03-06 22:59:32 -0300

    Fundamentos do .NET

--- C-Sharp/Testes/Testes/obj/Debug/net8.0/Testes.AssemblyInfo.cs
+++ C-Sharp/Testes/Testes/obj/Debug/net8.0/Testes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Testes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40987e9da4862afac252ef2418e1e543172d7c37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9aaf643e15470bc055673312a75c364d191fb0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Testes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Testes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
